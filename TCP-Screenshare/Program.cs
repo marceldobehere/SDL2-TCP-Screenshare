e34631672280c2ed9e2a7930147f0fc5a27ac1fc
Masl <marcel.z2006@gmail.com>
2022-02-13 00:29:58 +0100

    Projektdateien hinzufügen.

---
+++ TCP-Screenshare/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Veldrid.Sdl2;
+
+namespace TCP_Screenshare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Sdl2Window windows = new Sdl2Window("", 10, 10, 200, 100, SDL_WindowFlags.Resizable, true);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
